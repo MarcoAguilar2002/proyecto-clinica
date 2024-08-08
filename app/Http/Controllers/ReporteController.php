58fb0f820395fa40c43ec945e07cddda8d2e9b08
diegora16 <diegoeureka982@gmail.com>
2024-08-08 06:36:47 -0500

    actualizacion

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -8,13 +8,6 @@ use App\Models\Paciente;
 class ReporteController extends Controller
 {
     public function index(){
-        // Obtener distribución de pacientes por grupo sanguíneo -DR
-
-        $grupo_sanguineo_data = Paciente::select('grupo_sanguineo', \DB::raw('count(*) as total'))
-                                         ->groupBy('grupo_sanguineo')
-                                         ->get();
-
-                                         
-        return view('admin.reportes', compact('grupo_sanguineo_data'));
+        return view('admin.reportes');
     }
 }
