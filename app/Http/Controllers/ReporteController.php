c572b129d3bb8f14d7564dbaaf7112ce9586abc8
Joseph Gutierrez <t022700120@unitru.edu.pe>
2024-08-07 23:27:21 -0500

    reporte-bjgg agregado

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Pago;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 class ReporteController extends Controller
 {
-    //
+    public function index()
+    {
 
-    public function index(){
-        return view('admin.reportes');
+        $rangoFechas = Pago::select(
+            DB::raw('min(fecha_pago) as fecha_inicio'),
+            DB::raw('max(fecha_pago) as fecha_fin')
+        )->first();
+        $pagosTotales = Pago::select('doctors.especialidad', DB::raw('sum(pagos.monto) as total'))
+            ->join('pacientes', 'pagos.paciente_id', '=', 'pacientes.id')
+            ->join('doctors', 'pagos.doctor_id', '=', 'doctors.id')
+            ->whereBetween(
+                'pagos.fecha_pago',
+                [DB::raw('(SELECT min(p3.fecha_pago) FROM pagos p3)'), DB::raw('(SELECT max(p4.fecha_pago) FROM pagos p4)')]
+            )->groupBy('doctors.especialidad')
+            ->get();
+
+        return view('admin.reportes', compact('pagosTotales', 'rangoFechas'));
     }
 }
--- resources/views/admin/reportes/joseph.blade.php
+++ resources/views/admin/reportes/joseph.blade.php
@@ -1,60 +1,62 @@
-<div class="card  card-outline card-primary">
+<div class="card card-outline card-warning" id="reportBjgg">
     <div class="card-header">
-        <h3 class="card-title">Especialidad más solicitada con los días, con más pacientes en esa especialidad</h3>
+        @php
+            $fechaInicio = \Carbon\Carbon::parse($rangoFechas->fecha_inicio)->toFormattedDateString();
+            $fechaFin = \Carbon\Carbon::parse($rangoFechas->fecha_fin)->toFormattedDateString();
+        @endphp
+        <h3 class="card-title">
+            {{ __('Reporte de ganancias totales en S./ por Especialidad (' . $fechaInicio . ' hasta ' . $fechaFin . ')') }}
+        </h3>
         <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse">
                 <i class="fas fa-minus"></i>
             </button>
         </div>
     </div>
-    <div class="card-body" style="display: block;">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="chart-responsive">
-                    <div class="chartjs-size-monitor">
-                        <div class="chartjs-size-monitor-expand">
-                            <div class=""></div>
-                        </div>
-                        <div class="chartjs-size-monitor-shrink">
-                            <div class=""></div>
-                        </div>
-                    </div>
-                    <canvas id="pieChart3" height="300" width="300" style="display: block; width: 50%; height: auto;" class="chartjs-render-monitor"></canvas>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <ul class="chart-legend clearfix">
-                    <li><i class="far fa-circle text-danger"></i> Cardiología</li>
-                    <li><i class="far fa-circle text-success"></i> Dermatología</li>
-                    <li><i class="far fa-circle text-warning"></i> Gastroenterología</li>
-                    <li><i class="far fa-circle text-info"></i> Neurología</li>
-                    <li><i class="far fa-circle text-primary"></i> Pediatría</li>
-                    <li><i class="far fa-circle text-secondary"></i> Urología</li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <div id="chartdiv" style="width: 80%; height: 300px;"></div>
 </div>
+<script src="//cdn.amcharts.com/lib/5/index.js"></script>
+<script src="//cdn.amcharts.com/lib/5/percent.js"></script>
+<script src="//cdn.amcharts.com/lib/5/themes/Animated.js"></script>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        var ctx = document.getElementById('pieChart3').getContext('2d');
-        var pieChart3 = new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: ['Cardiología', 'Dermatología', 'Gastroenterología', 'Neurología', 'Pediatría', 'Urología'],
-                datasets: [{
-                    data: [30, 20, 10, 15, 10, 15],
-                    backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de']
-                }]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                legend: {
-                    display: false, // Oculta la leyenda generada por Chart.js
-                }
-            }
-        });
-    });
+    const pagosTotales = @json($pagosTotales);
+
+    // Create root and chart
+    var root = am5.Root.new("chartdiv");
+
+    root.setThemes([
+        am5themes_Animated.new(root)
+    ]);
+
+    var chart = root.container.children.push(
+        am5percent.PieChart.new(root, {
+            layout: root.horizontalLayout
+        })
+    );
+
+    // Create series
+    var series = chart.series.push(
+        am5percent.PieSeries.new(root, {
+            name: "Total",
+            valueField: "total",
+            categoryField: "especialidad",
+            legendLabelText: "[{fill}]{category}[/]",
+            legendValueText: "[bold {fill}]{value}[/]"
+        })
+    );
+    series.data.setAll(pagosTotales);
+    series.labels.template.set("forceHidden", true);
+    series.ticks.template.set("forceHidden", true);
+
+    // Add legend
+    var legend = chart.children.push(
+        am5.Legend.new(root, {
+            centerY: am5.percent(50),
+            y: am5.percent(50),
+            layout: root.verticalLayout
+        })
+    );
+
+    legend.data.setAll(series.dataItems);
 </script>
