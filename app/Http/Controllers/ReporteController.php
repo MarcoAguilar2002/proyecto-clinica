26a0a5e9cbbaa21ebf74e82eabf6dc160e973d09
diegora16 <diegoeureka982@gmail.com>
2024-08-07 23:35:52 -0500

    reporte-dara

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -9,10 +9,12 @@ class ReporteController extends Controller
 {
     public function index(){
         // Obtener distribución de pacientes por grupo sanguíneo -DR
+
         $grupo_sanguineo_data = Paciente::select('grupo_sanguineo', \DB::raw('count(*) as total'))
                                          ->groupBy('grupo_sanguineo')
                                          ->get();
 
+                                         
         return view('admin.reportes', compact('grupo_sanguineo_data'));
     }
 }
